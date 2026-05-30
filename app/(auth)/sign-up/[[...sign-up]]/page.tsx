51513eecd2954bdbb2c5190d16f410cd8fe8d478
satyam chaturvedi <ssatyamsharma5678@gmail.com>
2026-05-31 03:31:23 +0530

    project setup and authentication completed

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+import {SignUp } from "@clerk/nextjs"
+
+
+export default function Page() {
+  return (
+    <div className='flex flex-col items-center justify-center  min-h-screen dark: bg-neutral-800 text-neutral-900 dark:text-neutral-100'>
+    <h1 className='text-2xl font-bold mb-6'>welcome to treeBio</h1>
+    <p className='text-lg mb-4 font-semibold text-gray-500'> Please sign up to continue.</p>
+
+    <div className='mt-6'>
+        <SignUp />
+    </div>
+   </div>
+  )
+}
---
+++ lib/db.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from "./generated/prisma/client";
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const db = globalForPrisma.prisma || new PrismaClient({} as any);
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
