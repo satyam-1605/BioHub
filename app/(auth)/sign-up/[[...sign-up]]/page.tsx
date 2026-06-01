c28d989d3f3671e9e24e27bb138c656ef668cb62
satyam chaturvedi <ssatyamsharma5678@gmail.com>
2026-06-01 22:06:47 +0530

    working on project

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import {SignUp } from "@clerk/nextjs"
 export default function Page() {
   return (
     <div className='flex flex-col items-center justify-center  min-h-screen dark: bg-neutral-800 text-neutral-900 dark:text-neutral-100'>
-    <h1 className='text-2xl font-bold mb-6'>welcome to treeBio</h1>
+    <h1 className='text-2xl font-bold mb-6'>welcome to BioHub</h1>
     <p className='text-lg mb-4 font-semibold text-gray-500'> Please sign up to continue.</p>
 
     <div className='mt-6'>
--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,7 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient({} as any);
+export const db = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
